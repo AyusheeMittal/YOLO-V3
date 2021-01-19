a144ea85e864da314376329b486d05965b57a50e
Ayushee Mittal <ayusheemittal530@gmail.com>
2021-01-19 22:43:50 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,4 +1,3 @@
-#from utils.google_utils import *
 from utils.layers import *
 from utils.parse_config import *
 
@@ -334,6 +333,7 @@ class Darknet(nn.Module):
 
 def get_yolo_layers(model):
     return [i for i, m in enumerate(model.module_list) if m.__class__.__name__ == 'YOLOLayer']  # [89, 101, 113]
+
 
 def load_darknet_weights(self, weights, cutoff=-1):
     # Parses and loads the weights stored in 'weights'
