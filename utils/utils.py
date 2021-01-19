f341198b5a4adda01d3bd22c5ed7c0b67b66631c
Ayushee Mittal <ayusheemittal530@gmail.com>
2021-01-19 19:45:55 +0530

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -392,6 +392,7 @@ def compute_loss(p, targets, model):  # predictions, targets, model
     np, ng = 0, 0  # number grid points, targets
     for i, pi in enumerate(p):  # layer index, layer predictions
         b, a, gj, gi = indices[i]  # image, anchor, gridy, gridx
+        print("pi", pi)
         tobj = torch.zeros_like(pi[..., 0])  # target obj
         np += tobj.numel()
 
@@ -399,8 +400,9 @@ def compute_loss(p, targets, model):  # predictions, targets, model
         nb = len(b)
         if nb:  # number of targets
             ng += nb
-            print(b, a, gj, gi)
+            print("b, a, gj, gi", b, a, gj, gi)
             ps = pi[b, a, gj, gi]  # prediction subset corresponding to targets
+            print("ps", ps)
             # ps[:, 2:4] = torch.sigmoid(ps[:, 2:4])  # wh power loss (uncomment)
 
             # GIoU
